51c56275fc15a61df49a0112e4ab1542fb36d9db
jicc1127 <inouecowclinic@gmai.com>
2022-05-29 10:39:31 +0900

    my first function hsofpreg

---
+++ R/hsofpreg.R
@@ -0,0 +1,28 @@
+#' Count the heads of pregnancy
+#'
+#' @param DF Dataframe of the farms' reproduction data
+#'
+#' @return integer
+#' @export
+#'
+#' @examples
+#' DF
+#' hsofpreg <- hsofpreg(DF)
+#' hsofpreg
+
+
+hsofpreg <- function(DF){
+ #default of hsofpreg（heads of pregnancy)
+  #browser()
+  hsofpreg = 0
+ for(i in 1:nrow(DF)){
+    Group <- DF[i,2]   #get the value of Group
+    if(Group <= 4){
+      preg <- 0        #pregnancy -
+    }else{
+      preg <- 1        #pregnancy +
+    }
+    hsofpreg <- hsofpreg + preg
+  }
+  hsofpreg
+}
---
+++ data-raw/DF.R
@@ -0,0 +1,40 @@
+## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+library(readr)
+#Source: D:\csv\MH_20220313Cow00.csv
+
+DF <- readr::read_csv( "data-raw/MH_20220313Cow00.csv",
+                       col_types = list(vroom::col_integer(),
+                                        #LineNo
+                                        vroom::col_integer(),
+                                        #Group
+                                        vroom::col_character(),
+                                        #Stage
+                                        vroom::col_integer(),
+                                        #cowCode
+                                        vroom::col_character(),
+                                        #cowGroup
+                                        vroom::col_character(),
+                                        #cowidNo
+                                        vroom::col_date( format = "%Y/%m/%d" ),
+                                        #birthday
+                                        vroom::col_integer(),  #parity
+                                        vroom::col_date( format = "%Y/%m/%d" ),
+                                        #calving_date
+                                        vroom::col_integer(),  #daysfrmcalving
+                                        vroom::col_integer(),  #AItimes
+                                        vroom::col_date( format = "%Y/%m/%d" ),
+                                        #firstAI_date
+                                        vroom::col_integer(),
+                                        #fstAIdaysfrmcalving
+                                        vroom::col_date( format = "%Y/%m/%d" ),
+                                        #lastAI_date
+                                        vroom::col_integer(),  #daysfrmlstAI
+                                        vroom::col_date( format = "%Y/%m/%d" ),
+                                        #expDateofnextCalving
+                                        vroom::col_integer(),
+                                        #opendays
+                                        vroom::col_date( format = "%Y/%m/%d" )))
+#base_date
+
+usethis::use_data(DF, overwrite = TRUE)
