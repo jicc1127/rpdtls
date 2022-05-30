224d829a84dbf7b6682195952f1c5b1c55611de9
jicc1127 <inouecowclinic@gmai.com>
2022-05-30 10:01:36 +0900

    add function nof_inkpr_ai

--- R/nof_unkpr_ai.R
+++ R/nof_unkpr_ai.R
@@ -1,6 +1,6 @@
 #' Count the number of unknown pregnancy AI
 #'
-#'This return is the total of unknown pregnant Cows' AItimes
+#' This return is the number of the nonpregnant AI
 #'
 #' @param DF Dataframe of the farms' reproduction data
 #'
@@ -8,23 +8,23 @@
 #' @export
 #'
 #' @examples
-#' nofunkpr_ai <- nofunkpr_ai(DF)
-#' nofunkpr_ai
+#' nof_unkpr_ai <- nof_unkpr_ai(DF)
+#' nof_unkpr_ai
 
 
-nofunkpr_ai <- function(DF){
+nof_unkpr_ai <- function(DF){
 
-  #default of nofunkpr_ai(the number of unknown pregnancy AItimes)
-  nofunkpr_ai = 0
+  #default of nof_unkpr_ai(the number of unknown pregnancy AItimes)
+  nof_unkpr_ai = 0
   for(i in 1:nrow(DF)){
-    AItimes <- DF[i,11]  #get the value of AItimes
+    #AItimes <- DF[i,11]  #get the value of AItimes
     Group <- DF[i,2]   #get the value of Group
     if(Group == 4){
       unkpr <- 1        #unknown pregnancy
     }else{
       unkpr <- 0        #pregnant or nonpregnant
     }
-    nofunkpr_ai <- nofunkpr_ai + AItimes*unkpr
+    nof_unkpr_ai <- nof_unkpr_ai + unkpr
   }
-  nofunkpr_ai <- as.numeric(nofunkpr_ai)
+  nof_unkpr_ai <- as.numeric(nof_unkpr_ai)
 }
