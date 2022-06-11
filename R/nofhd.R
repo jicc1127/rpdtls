2848fb6b52ee1573949d7992c1c26605e4fcaf90
jicc1127 <56163768+jicc1127@users.noreply.github.com>
2022-06-11 09:59:47 +0900

    add function nofhd

---
+++ R/nofhd.R
@@ -0,0 +1,15 @@
+#' The number of head
+#'
+#' @param DF Dataframe of the farms' reproduction data
+#'
+#' @return int The number of head
+#' @export
+#'
+#' @examples
+#' nofhd <- nofhd(DF)
+#' nofhd
+#'
+nofhd <- function(DF){
+
+  nofhd <- nrow(DF) #get the number of head
+}
